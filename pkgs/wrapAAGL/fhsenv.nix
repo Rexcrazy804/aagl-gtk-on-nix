3c924f450baa0e50e8a6b634e005ccde6b56e533
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-05-09 23:02:32 +0400

    added extra gstreamer stuff to fhsenv in case that works

--- pkgs/wrapAAGL/fhsenv.nix
+++ pkgs/wrapAAGL/fhsenv.nix
@@ -183,9 +183,15 @@ in buildFHSEnv rec {
     # Not formally in runtime but needed by some games
     at-spi2-atk
     at-spi2-core   # CrossCode
+    gstreamer
     gst_all_1.gstreamer
-    gst_all_1.gst-plugins-ugly
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
+
     json-glib # paradox launcher (Stellaris)
     libdrm
     libxkbcommon # paradox launcher
